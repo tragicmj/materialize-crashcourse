c7cb1d14dfe81dcae13816808d97c3884518030e
javed432 <javed.akhtar432@gmail.com>
2020-04-18 00:46:59 +0530

    Slider Added

--- js/loader.js
+++ js/loader.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
   $(".modal").modal();
   $(".parallax").parallax();
   $(".sidenav").sidenav();
+  $(".slider").slider({
+    full_width: true,
+  });
 });
 
 function toggleModal() {
